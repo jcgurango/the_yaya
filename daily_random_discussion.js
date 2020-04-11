50208a4126438b0fbefd0330f1119887b710a2e6
JC Gurango <jc@jcgurango.com>
2020-04-11 16:29:36 +0800

    Put RDs in a function

--- daily_random_discussion.js
+++ daily_random_discussion.js
@@ -3,47 +3,49 @@ var config   = require('./lib/config');
 var getQuote = require('./lib/quote');
 var reddit   = require('./lib/reddit');
 
-// the actual post to be submitted
-//
-var submission = {
-  'title': strftime('Daily random discussion - %b %d, %Y'),
-  'text': config.slogan,
-  'r': config.defaults.subreddit,
-  'inboxReplies': false,
-  'save': false,
-};
+module.exports = () => new Promise(function(resolve, reject) {
+  // the actual post to be submitted
+  //
+  var submission = {
+    'title': strftime('Daily random discussion - %b %d, %Y'),
+    'text': config.slogan,
+    'r': config.defaults.subreddit,
+    'inboxReplies': false,
+    'save': false,
+  };
 
-// first argument from the command line can be used to
-// override the subreddit that we are submitting to
-//
-if (process.argv[2]) {
-  submission.r = process.argv[2];
-}
-
-var silent = "Philippines" !== submission.r;
-
-getQuote(silent, function(quote) {
-  if (quote) {
-    submission.text += quote + '\n\n';
+  // first argument from the command line can be used to
+  // override the subreddit that we are submitting to
+  //
+  if (process.argv[2]) {
+    submission.r = process.argv[2];
   }
-  submission.text += strftime('Happy %A!!');
 
-  reddit.submit(submission, function(err, id) {
-    if (err) {
-      error(err);
-    } else {
-      exit('submitted ' + id);
+  var silent = "Philippines" !== submission.r;
+
+  getQuote(silent, function(quote) {
+    if (quote) {
+      submission.text += quote + '\n\n';
     }
+    submission.text += strftime('Happy %A!!');
+
+    reddit.submit(submission, function(err, id) {
+      if (err) {
+        error(err);
+      } else {
+        exit('submitted ' + id);
+      }
+    });
   });
+
+  // in case of error, log to STDERR and exit
+  //
+  function error(err) {
+    reject(err);
+  }
+
+  function exit(msg) {
+    console.log(strftime('%F %T ') + msg);
+    resolve();
+  }
 });
-
-// in case of error, log to STDERR and exit
-//
-function error(err) {
-  exit('ERROR: ' + err);
-}
-
-function exit(msg) {
-  console.log(strftime('%F %T ') + msg);
-  process.exit();
-}
--- nightly_random_discussion.js
+++ nightly_random_discussion.js
@@ -3,65 +3,65 @@ var request  = require('request-json');
 var config   = require('./lib/config');
 var reddit   = require('./lib/reddit');
 
-// first argument from the command line can be used to
-// override the subreddit that we are submitting to
-//
-if (process.argv[2]) {
-  config.defaults.subreddit = process.argv[2];
-}
+module.exports = () => new Promise(function(resolve, reject) {
+  // first argument from the command line can be used to
+  // override the subreddit that we are submitting to
+  //
+  if (process.argv[2]) {
+    config.defaults.subreddit = process.argv[2];
+  }
 
-// the actual post to be submitted
-//
-var submission = {
-  'title': strftime('Nightly random discussion - %b %d, %Y'),
-  'text': config.slogan + 'Magandang gabi r/' + config.defaults.subreddit + '!',
-  'r': config.defaults.subreddit,
-  'inboxReplies': false,
-  'save': false,
-};
+  // the actual post to be submitted
+  //
+  var submission = {
+    'title': strftime('Nightly random discussion - %b %d, %Y'),
+    'text': config.slogan + 'Magandang gabi r/' + config.defaults.subreddit + '!',
+    'r': config.defaults.subreddit,
+    'inboxReplies': false,
+    'save': false,
+  };
 
-reddit.submit(submission, function(err, id) {
-  if (err) {
-    error(err);
-  } else {
-    log('submitted ' + id);
+  reddit.submit(submission, function(err, id) {
+    if (err) {
+      error(err);
+    } else {
+      log('submitted ' + id);
 
-    var client = request.newClient('http://www.reddit.com/'),
-        url = '/r/AskReddit/top/.json';
+      var client = request.newClient('http://www.reddit.com/'),
+          url = '/r/AskReddit/top/.json';
 
-    client.get(url, function(err, res, body) {
-      if (!err) {
-        try {
-          post = body.data.children[0].data
-        } catch(e) {}
-        if (post) {
-          var text = 'Today\'s [Ask PHreddit](' + post.url + '): '
-                   + post.title;
-          reddit.comment('t3_' + id, text, function(err, id) {
-            if (err) {
-              error(err);
-            } else {
-              exit('commented ' + id);
-            }
-          });
+      client.get(url, function(err, res, body) {
+        if (!err) {
+          try {
+            post = body.data.children[0].data
+          } catch(e) {}
+          if (post) {
+            var text = 'Today\'s [Ask PHreddit](' + post.url + '): '
+                    + post.title;
+            reddit.comment('t3_' + id, text, function(err, id) {
+              if (err) {
+                error(err);
+              } else {
+                exit('commented ' + id);
+              }
+            });
+          }
         }
-      }
-    });
+      });
 
+    }
+  });
+
+  function error(err) {
+    reject(err);
+  }
+
+  function log(msg) {
+    console.log(strftime('%F %T ') + msg);
+  }
+
+  function exit(msg) {
+    log(msg);
+    resolve();
   }
 });
-
-// in case of error, log to STDERR and exit
-//
-function error(err) {
-  exit('ERROR: ' + err);
-}
-
-function log(msg) {
-  console.log(strftime('%F %T ') + msg);
-}
-
-function exit(msg) {
-  log(msg);
-  process.exit();
-}
