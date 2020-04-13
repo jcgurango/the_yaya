94f6db5fae48fe319a189c87b81b2ec173b54712
JC Gurango <jc@jcgurango.com>
2020-04-13 20:35:45 +0800

    Use UTC timezone

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var cron = require('node-cron');
 
-cron.schedule('55 5  * * *', require('./morning_random_discussion'));
-cron.schedule('55 11 * * *', require('./afternoon_random_discussion'));
-cron.schedule('55 17 * * *', require('./evening_random_discussion'));
-cron.schedule('55 17 * * *', require('./nightly_random_discussion'));
+cron.schedule('55 21  * * *', require('./morning_random_discussion'));
+cron.schedule('55 3 * * *', require('./afternoon_random_discussion'));
+cron.schedule('55 9 * * *', require('./evening_random_discussion'));
+cron.schedule('55 15 * * *', require('./nightly_random_discussion'));
 cron.schedule('39 2 1 * *', require('./monthly_what_to_do'));
 
 console.log('CRONs scheduled.');
